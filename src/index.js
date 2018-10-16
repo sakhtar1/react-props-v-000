d2a2a030021e6b36d1476bce5ff8e8e762be2d30
Sharmina Akhtar <sakht318@gmail.com>
2018-10-16 00:24:02 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 // Code goes here
+const madMaxGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+const titles = ['Mad Max: Fury Road']
+
+
+ReactDOM.render(
+  <MovieCard
+    title={titles}
+    poster="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg"
+    genres={madMaxGenres}
+  />,
+  document.getElementById('root')
+);
