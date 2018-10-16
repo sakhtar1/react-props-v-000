bc995a5772a28f35a472b101c56510e026800749
Sharmina Akhtar <sakht318@gmail.com>
2018-10-16 00:24:02 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 // Code goes here
+MovieCard.defaultProps = {
+  poster: 'http://i.imgur.com/bJw8ndW.png'
+};
+
 const madMaxGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
 const titles = ['Mad Max: Fury Road']
 
@@ -11,3 +15,19 @@ ReactDOM.render(
   />,
   document.getElementById('root')
 );
+
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <img  
+          src={this.props.poster} 
+          alt={this.props.title} 
+        />
+        <h2>{this.props.title}</h2>
+        <small>Genres: {this.props.genres.join(', ')}</small>
+      </div>
+    );
+  }
+}
